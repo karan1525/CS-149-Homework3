afcf008cd51588eb658c56f5559703f61d3b3af8
michaelhyun <michael.hyun3@gmail.com>
2017-03-10 13:48:23 -0800

    Created Initial Classes with no implementation

---
+++ Homework3/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main implements Runnable{
+
+	Thread runnable;
+	
+	public Main(){
+//		this.runnable = new Thread(this);
+//		runnable.start();
+	}
+	
+	public static void main(String[] args){
+		new Main();
+	}
+	
+	
+	@Override
+	public void run() {
+		
+	}
+
+	
+	
+}
